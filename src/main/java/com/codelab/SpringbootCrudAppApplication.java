a32a0ff14422203a07542a7229b8b5b5e9833aa2
maroti Jadhav <tech.maroti92@gmail.com>
2024-04-27 00:30:58 +0530

    second commit, added swagger api for documentation

--- src/main/java/com/codelab/SpringbootCrudAppApplication.java
+++ src/main/java/com/codelab/SpringbootCrudAppApplication.java
@@ -1,9 +1,24 @@
 package com.codelab;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Employee-Service",
+		version = "1.0.0",
+		description = "Employees Management Service Information"
+),
+		servers = @Server(
+				url = "http://localhost:9091",
+				description = "Employee Rest Api"
+
+		)
+
+)
 public class SpringbootCrudAppApplication {
 
 	public static void main(String[] args) {
